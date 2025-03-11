e1408fcff757962360b368049db7bf122c60cd07
punkharb <ppbthunder@gmail.com>
2025-03-11 15:45:49 +0700

    Add files via upload

---
+++ week1/reactlab/componentlab/sec.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState } from "react";
+
+export default function List(){
+    const [items, setItems] = useState([]);
+    return <div>
+        
+    </div>
+}
---
+++ week1/reactlab/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Add from "../componentlab/first"
+import './App.css'
+
+export default function App() {
+  return <div>
+    <h1>Simple Calculator</h1>
+    <Add/>
+  </div>
+   
+}
+
