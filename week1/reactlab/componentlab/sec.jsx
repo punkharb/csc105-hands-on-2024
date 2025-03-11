0fd7648bbe93db0c0a959442bfe5e09b90f85b56
punkharb <ppbthunder@gmail.com>
2025-03-11 15:46:26 +0700

    Delete week1/reactlab directory

--- week1/reactlab/componentlab/sec.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useState } from "react";
-
-export default function List(){
-    const [items, setItems] = useState([]);
-    return <div>
-        
-    </div>
-}
