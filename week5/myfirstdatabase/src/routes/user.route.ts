defb5b12348d48549803a37842a940ba211f780c
punkharb <ppbthunder@gmail.com>
2025-04-22 14:35:09 +0700

    Delete week5/myfirstdatabase directory

--- week5/myfirstdatabase/src/routes/user.route.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-import * as userController from "../controllers/user.controller.ts";
-
-const userRouter = new Hono();
-
-userRouter.post("/", userController.createUser);
-userRouter.patch("/", userController.updateUser);
-
-export { userRouter };
